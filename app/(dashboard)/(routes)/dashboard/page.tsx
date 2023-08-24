409f2939ca276c1d88bd32011100b223cce1dadc
arifnoviarrizal <noviarrizalarif@gmail.com>
2023-08-24 16:18:31 +0700

    setup clerk

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function DashboardPage() {
   return (
     <>
-    <p className="text-6xl text-green-500">Halo ai sass</p>
-    <Button variant="destructive">Click</Button>
+      <p>Dashboard Page</p>
+      <UserButton afterSignOutUrl="/" />
     </>
-  )
+  );
 }

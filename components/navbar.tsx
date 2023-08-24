ddcdc366686bcf186cb7613f7cff514909501160
arifnoviarrizal <noviarrizalarif@gmail.com>
2023-08-24 19:45:30 +0700

    adding components sidebar mobile and sheets

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,14 @@
 import { Menu } from "lucide-react";
 import { Button } from "./ui/button";
 import { UserButton } from "@clerk/nextjs";
+import MobileSidebar from "./mobile-sidebar";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
   return (
     <div className="flex items-center p-4">
-      <Button variant="ghost" size="icon" className="md:hidden">
-        <Menu />
-      </Button>
+      <MobileSidebar />
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
       </div>

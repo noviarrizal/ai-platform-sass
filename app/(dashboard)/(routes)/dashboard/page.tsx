53360ebc75f88b64ea13e70a647353fa872c0f34
arifnoviarrizal <noviarrizalarif@gmail.com>
2023-08-24 15:08:33 +0700

    changing the folder route structure

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function DashboardPage() {
+  return (
+    <>
+    <p className="text-6xl text-green-500">Halo ai sass</p>
+    <Button variant="destructive">Click</Button>
+    </>
+  )
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,7 @@
+const LandingPage = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default LandingPage
